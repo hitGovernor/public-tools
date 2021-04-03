9d76143695f900c86c8f8fcb7eed6fcd92698c3f
Brian Johnson <bjohnson@evolytics.com>
2021-04-02 21:25:14 -0500

    add getCookie

--- tealium/tiq_utils.js
+++ tealium/tiq_utils.js
@@ -19,3 +19,27 @@ if (document.cookie.indexOf("utagdb=true") > -1) {
 
 // display ccpa popup modal
 utag.gdpr.showDoNotSellPrompt("en-US");
+
+
+/**
+ * checks for cookie value in utag datalayer before looking for the actual cookie
+ * @param c_name {string} - Name of cookie to locate
+ * @param do_update {=Boolean} - If true, updates utag datalayer value with value found in cookie; if false/undefined, does not
+ */
+function getCookie(c_name, do_update) {
+  var b = b || {},
+    retval = undefined,
+    utag_cookie = b["cp." + c_name],
+    browser_cookie = document.cookie.match(new RegExp(c_name + '=([^;]+)'))[1];
+
+  if (browser_cookie) {
+    retval = browser_cookie;
+    if (do_update) {
+      b["cp." + c_name] = browser_cookie;
+    }
+  } else {
+    retval = utag_cookie;
+  }
+
+  return retval;
+}
