4dd3108296a6028cf3f17a9062ff95fc432b25c4
Brian Johnson <bjohnson@evolytics.com>
2023-01-12 08:24:16 -0600

    expand project name wrap selector

--- misc/px_hacks.js
+++ misc/px_hacks.js
@@ -1,7 +1,7 @@
 // PX: Wrap Project Names
 // bookmarklet: 
-// javascript:(function()%7Bdocument.querySelectorAll("%5Bdata-testid%3D%27MatchedText%27%5D").forEach(function(item)%7Bitem.setAttribute("style","white-space:normal")%3B%7D)%3B%7D)()%3B
+// javascript:(function()%7Bdocument.querySelectorAll(%22%5Bdata-testid%3D%27MatchedText%27%5D,%20%5Bdata-testid%3D%27UiTypography%27%5D%22).forEach(function(item)%7Bitem.setAttribute(%22style%22,%22white-space:normal%22)%3B%7D)%3B%7D)()%3B
 
-document.querySelectorAll("[data-testid='MatchedText']").forEach(function (item) {
+document.querySelectorAll("[data-testid='MatchedText'], [data-testid='UiTypography']").forEach(function (item) {
   item.setAttribute("style", "white-space:normal");
 });
