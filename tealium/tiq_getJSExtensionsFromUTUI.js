c4ed3a3e6d9941fd3fd6a49172a3683130278c77
Brian Johnson <bjohnson@evolytics.com>
2025-01-22 18:46:33 -0600

    add csv formatting and download logic

--- tealium/tiq_getJSExtensionsFromUTUI.js
+++ tealium/tiq_getJSExtensionsFromUTUI.js
@@ -1,4 +1,52 @@
-let profileOverride = [];
+let profileOverride = []; // if you want to specify pofiles, define here, otherwise leave empty array
+
+let formatForCSV = function(payload) {
+  let decodedCode = payload.replace(/&quot;/g, '"'); 
+  let escapedCode = decodedCode.replace(/"/g, '\\"').replace(/\n/g, '\\n')//.replace(/\t/g, '\\t');
+  let quotedCode = `"${escapedCode}"`; // wrap the entire code block in double quotes
+  return quotedCode;
+}
+
+let download = function (data, account, suffix) {
+  tiq_account = account || "";
+  let filename = 'tiq-inventory-' + tiq_account + ((suffix) ? "-" + suffix : "") + ".csv"
+  const blob = new Blob([data], {
+    type: 'text/csv'
+  });
+
+  // create object for download url
+  const url = window.URL.createObjectURL(blob)
+  const a = document.createElement('a')
+  a.setAttribute('href', url);
+  a.setAttribute('download', filename);
+
+  // download the file
+  a.click();
+}
+
+let convertToCSV = function (assets) {
+  let allBody = [];
+  let headers = [];
+
+  // build the csv header row based on keys in first asset
+  headers = Object.keys(assets[0]).join(",") + "\n";
+
+  // loop throug all assets to build the individual rows
+  for (let item in assets) {
+    let tmp = assets[item],
+      tmpArray = [];
+
+    for (let key in tmp) {
+      tmpArray.push(tmp[key]);
+    }
+
+    allBody.push(tmpArray.join(","));
+  }
+
+  // join everything together w/ line breaks between asset rows
+  return headers + allBody.join("\n");
+}
+
 utui.automator.getAllProfiles(utui.login.account).then(function (profiles) {
   let extensions = [];
   let results = { profiles: 0 }
@@ -25,9 +73,8 @@ utui.automator.getAllProfiles(utui.login.account).then(function (profiles) {
         let ajs_extensions = extensions.filter((item) => item.extType == "Advanced Javascript Code");
         exts = js_extensions.concat(ajs_extensions);
 
-        // console.log(exts);
-
         let output = [];
+        let csvOutput = [];
         exts.forEach(function (item) {
           let code = (function (asset) {
             if (asset?.codeDevData?.promotedSnippets) {
@@ -52,8 +99,20 @@ utui.automator.getAllProfiles(utui.login.account).then(function (profiles) {
             scope: item.advExecOption,
             code: code
           });
+
+          csvOutput.push({
+            profile: item.profile,
+            type: item.extType,
+            status: item.status,
+            uid: item._id,
+            title: item.title,
+            scope: item.advExecOption,
+            code: formatForCSV(code || "")
+          });
         });
+        
         console.log(output);
+        download(convertToCSV(csvOutput),(utui.login.account || ""), "js-extensions");
       }
     });
   });
